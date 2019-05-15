9852030c7c5bf7b1acf86a627ad9a8c04e0dbf54
Ken Irwin <kirwin@wittenberg.edu>
2019-05-15 13:35:49 -0400

    added config sample

---
+++ config_sample.php
@@ -0,0 +1,8 @@
+<?php
+define('DB','');
+define('HOST','localhost');
+define('USER','');
+define('PASS','');
+define('CHARSET','utf8');
+define('DSN','mysql:host='.HOST.';dbname='.DB.';charset='.CHARSET);
+?>
