38df9a97ad391111ddcdf5fb6925262f7f1b8d8d
Ken Irwin <kirwin@wittenberg.edu>
2019-05-15 16:52:02 -0400

    update_genrelist - use current filepath

--- update_genrelist.sh
+++ update_genrelist.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 
-
-cd /docs/lib/research/av/
+cd /docs/lib/find/av/
 rm feature_film_lookup.html.bak
 mv feature_film_lookup.html feature_film_lookup.html.bak
 php feature_film_lookup.php > feature_film_lookup.html
